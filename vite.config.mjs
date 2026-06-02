e8c0a86d60d269d8d5916b087e6294ba2ec84f09
Elnur Zhunussov <zhunusov.e@applecity.kz>
2026-06-02 15:01:40 +0500

    Fix: github deploy

--- vite.config.mjs
+++ vite.config.mjs
@@ -2,11 +2,11 @@ import basicSsl from '@vitejs/plugin-basic-ssl';
 import vue from '@vitejs/plugin-vue';
 import { defineConfig } from 'vite';
 
-export default defineConfig({
-    base: '/qr-code-github/',
+export default defineConfig(({ command }) => ({
+    base: command === 'build' ? '/qr-code-github/' : '/',
     plugins: [vue(), basicSsl()],
     server: {
         port: 3000,
         host: '0.0.0.0'
     }
-});
+}));
